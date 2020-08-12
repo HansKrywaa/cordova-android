c6bc985dcebcf5e4f6773b690b231aba28e46596
HansKrywaa <hanskrywaa@web.de>
2020-08-12 10:10:55 +0200

    Switch to AppCompatActivity

--- framework/src/org/apache/cordova/CordovaInterface.java
+++ framework/src/org/apache/cordova/CordovaInterface.java
@@ -18,9 +18,10 @@
 */
 package org.apache.cordova;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.apache.cordova.CordovaPlugin;
 
@@ -56,7 +57,7 @@ public interface CordovaInterface {
      *
      * @return the Activity
      */
-    public abstract Activity getActivity();
+    public abstract AppCompatActivity getActivity();
 
     /**
      * Get the Android context.
